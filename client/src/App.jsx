4623d40cac518530a121c056701afc1b9fc75f3c
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-07-18 17:10:36 +0300

    create game added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Login from './components/users/Login.jsx'
 import Register from './components/users/Register'
 import Home from "./components/Home"
 import Catalog from './components/Catalog.jsx'
+import GameCreate from './components/GameCreate'
 import {Routes,Route} from 'react-router-dom'
 function App() {
 
@@ -15,7 +16,8 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
-        <Route path="all-games" element={<Catalog/>}/>
+        <Route path="/all-games" element={<Catalog/>}/>
+        <Route path='/create-game' element={<GameCreate/>}/>
       </Routes>
     </div>
   )
