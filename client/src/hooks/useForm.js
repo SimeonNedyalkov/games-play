f3017158f3c59ffc7f97845d15170783f2fffde0
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-07-25 12:09:13 +0300

    useForm created

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export function useForm(initialValue,submitCallBack){
+    const [values,setValues] = useState(initialValue)
+
+    const changeHandler = (e) =>{
+        setValues(state=>({
+            ...state,
+            [e.target.name] : e.target.value
+        }))
+    }
+    
+    const submitHandler = (e) =>{
+        e.preventDefault()
+        submitCallBack(values)
+    }
+
+    return {values,changeHandler,submitHandler}
+}
