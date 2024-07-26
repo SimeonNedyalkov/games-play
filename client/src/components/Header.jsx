40bbde8a392ad6c3c62ff9016a9a8a55f59c1d7e
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-07-26 13:49:47 +0300

    header navbar look changed for user and guest

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,24 @@
+import { useContext } from 'react'
 import {Link} from 'react-router-dom'
+import UserContext from '../contexts/userContext'
 export default function Header(){
+    const {isAuthenticated} = useContext(UserContext)
     return (
         <header>
             <h1><Link className="home" to="/">GamesPlay</Link></h1>
             <nav>
                 <Link to="/all-games">All games</Link>
-                <div id="user">
+                {isAuthenticated ?(
+                    <div id="user">
                     <Link to="/create-game">Create Game</Link>
                     <Link to="/logout">Logout</Link>
                 </div>
-                <div id="guest">
+                ):(
+                    <div id="guest">
                     <Link to="/login">Login</Link>
                     <Link to="/register">Register</Link>
-                </div>
+                    </div>
+                )}
             </nav>
         </header>
     )
--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -5,16 +5,18 @@ import UserContext from "../contexts/userContext"
 export function useLogin(){
     const {changeAuthState} = useContext(UserContext)
     async function loginHandler(email,password){
-        const result = await userAPI.login(email,password)
+        const {password:_ , ...result} = await userAPI.login(email,password)
         changeAuthState(result)
+        return result
     }
     return loginHandler
 }
 export function useRegister(){
     const {changeAuthState} = useContext(UserContext)
     async function registerHandler(email,password){
-        const result = await userAPI.register(email,password)
+        const {password:_ , ...result} = await userAPI.register(email,password)
         changeAuthState(result)
+        return result
     }
     return registerHandler
 }
